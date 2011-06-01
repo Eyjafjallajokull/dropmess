fea7f06f2d01d1cfbabfa6aa0660ea44fe41a81e
Eyjafjallajokull <pawel.olejniczak@gmail.com>
2011-06-01 22:15:34 +0200

    drop mimetypes detection; cleanup

--- dropmess.py
+++ dropmess.py
@@ -1,39 +1,55 @@
-import core.config as config
-import mimetypes
+import ConfigParser
 import os
 import time
+
+fileTypes = {
+  'Applications' : [ 'exe', 'bin', 'sh', 'deb' ],
+  
+  'Music' : [ 'mp3', 'mp2', 'wav', 'rm', 'flac' ],
+  
+  'Video' : [ 'avi', 'mpg', 'mov', 'mp4', 'ogv', 'flv' ],
+  
+  'Documents' : [
+    # ms
+    'doc', 'xls', 'ppt',
+    # oo
+    'odt', 'odp',
+    # other
+    'psd', 'xfc', 
+    'txt', 'pdf'
+  ],
+  
+  'Compressed' : [ 'tar', 'rar', 'zip', 'gz', '7z' ],
+  
+  'Images' : [ 'jpg', 'png', 'gif', 'jpeg', 'tiff', 'raw', 'nef', 'svg' ],
+  
+  'Sources' : [
+    'rb', 'py', 'c', 'java', 'php', 
+    'css', 'html', 'js'
+  ],
+  
+  'Unknown' : [],
+  'Dictionary' : []
+}
 
 dirs = []
 diffs = {}
 
-types = {}
-for (ext,type) in mimetypes.types_map.items():
-    types[type[0:type.find('/')]] = 1
-types = types.keys()
-types.append('dir')
-types.append('unknown')
+configFile = './dropmess.ini'
+config = None
 
-def dropFile(filename):
-    pass
-
-def dropDir(dirname):
-    pass
-
-def detectMime(name):
+def detectType(name):
     if (os.path.isfile(name)):
-        mime = mimetypes.guess_type(name)[0]
-        if mime == None:
-            return 'unknown';
-        return mime[0 : mime.find('/')]
-    
+        ext = os.path.splitext(name)[1][1:]
+        for (name, exts) in fileTypes.items():
+            if ext in exts:
+                return name
+        return 'Unknown';
     elif (os.path.isdir(name)):
-        return 'dir';
-        for root, dirs, files in os.walk(name):
-            for file in files:
-                pass
+        return 'Dictionary';
 
 def move(src, dst, attempt = 0):
-    #print src,dst,attempt
+    #print src,dst,attempt 
     tmpdst = dst
     if attempt > 0:
         p1 = dst.rfind('.')
@@ -46,7 +62,6 @@ def move(src, dst, attempt = 0):
             tmpdst = a + b + c
     
     try:
-        
         if os.path.isfile(tmpdst):
             raise OSError
         os.rename(src, tmpdst)
@@ -54,22 +69,23 @@ def move(src, dst, attempt = 0):
         move(src, dst, attempt+1)
 
 def dropMess(name):
-    global diffs
     diff = diffs[name] = os.listdir(name)
     for entry in diff:
-        if entry in types:
+        if entry in fileTypes.keys():
             continue
         path = os.path.join(name, entry)
-        mime = detectMime(path)
-        targetDir = os.path.join(name, mime) + os.sep
+        ftype = detectType(path)
+        targetDir = os.path.join(name, ftype) + os.sep
         if not os.path.isdir(targetDir):
             os.mkdir(targetDir)
         target = os.path.join(targetDir, entry)
         move(path, target, 0)
 
 if __name__ == '__main__':
-    config.load()
-    dirs = config.config.sections()
+    config = ConfigParser.ConfigParser()
+    config.readfp(file(os.path.expandvars(configFile)))
+    dirs = config.sections()
+    
     print dirs
     
     while True:
